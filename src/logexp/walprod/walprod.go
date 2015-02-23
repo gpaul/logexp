2ac0cae523218fed176428a5446f9a4adde007ae
Gustav Paul <gustav.paul@gmail.com>
2015-02-22 20:47:19 -0800

    walprod: wait 1 second before generating next 1000 batch. these constants should be flags

--- src/logexp/walprod/walprod.go
+++ src/logexp/walprod/walprod.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"time"
-	"log"
-	"logexp"
-	"strconv"
 	"encoding/json"
 	"flag"
+	"log"
+	"logexp"
 	"net"
+	"strconv"
+	"time"
 )
-	
+
 func main() {
 	dialpath := flag.String("dialpath", "@/wal", "the unix socket to dial to")
 
@@ -23,16 +23,19 @@ func main() {
 
 	last := 0
 	start := time.Now()
-	for i:=0; ; i++ {
+	for i := 0; ; i++ {
 		msg := logexp.Message{"hello " + strconv.Itoa(i)}
 		if err := enc.Encode(msg); err != nil {
 			panic(err)
 		}
 		elapsed := time.Now().Sub(start)
 		if elapsed > time.Second {
-			log.Printf("%d msgs/s", int(float64(i-last) / elapsed.Seconds()))
+			log.Printf("%d msgs/s", int(float64(i-last)/elapsed.Seconds()))
 			start = time.Now()
 			last = i
+		}
+		if i%1000 == 0 {
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
